172351aa4a2e0f16cae618a6b04666b8441ff56d
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-24 15:38:37 +0530

    Redux toolkit for counter app

---
+++ Redux Tool Kit/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Redux tool kit</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
