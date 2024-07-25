27a349755f3b74706f694a48db87f08ca793da26
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-25 15:09:01 +0530

    counter revision done

--- Counter/src/Component/Counter/Counter.jsx
+++ Counter/src/Component/Counter/Counter.jsx
@@ -1,59 +1,51 @@
 import React, { useState } from "react";
-
 const Counter = () => {
-  const [count, setCount] = useState(0);
-  //   const increment = () => {
-  //     setCount(count + 1);
-  //   };
-  //   const decrement = () => {
-  //     if (count > 0) {
-  //       setCount(count - 1);
-  //     }
-  //   };
-  //   const reset = () => {
-  //     setCount(0);
-  //   };
+  const [counter, setCounter] = useState(0);
+  // const increment = () => {
+  //   setCounter(counter + 1);
+  // };
+  // const decrement = () => {
+  //   if (counter > 0) {
+  //     setCounter(counter - 1);
+  //   }
+  // };
+  // const reset = () => {
+  //   setCounter("0");
+  // };
   return (
     <>
-      <div className="bg-yellow-400 min-h-screen">
-        <div className="py-24">
-          <div className="py-12 bg-red-700 h-96 w-96 mx-auto rounded-lg">
-            <div className="bg-gray-400 h-52 w-52 mx-auto content-center rounded-xl">
-              <h1 className="text-3xl font-bold text-gray-650 text-center">
-                {count}
-              </h1>
-            </div>
-            <div className="flex justify-center p-3 gap-4">
-              <button
-                className="bg-red-300 rounded-full p-2 font-bold"
-                onClick={() => {
-                  if (count > 0) {
-                    setCount(count - 1);
-                  }
-                }}
-              >
-                Decrement(-)
-              </button>
-              <button
-                className="bg-green-300 rounded-full p-2 font-bold"
-                onClick={() => setCount(count + 1)}
-              >
-                Increment(+)
-              </button>
-            </div>
-            <div className="text-center">
-              <button
-                className="bg-black text-white rounded-2xl p-2 font-bold"
-                onClick={() => setCount(0)}
-              >
-                Reset
-              </button>
-            </div>
+      <div className="min-h-screen bg-slate-900 flex flex-col justify-center items-center">
+        <h1 className="text-white text-3xl font-bold">Counter</h1>
+        <div className="bg-slate-700">
+          <div className="flex h-96 w-96 items-center">
+            <p className=" mx-auto text-3xl">{counter}</p>
+          </div>
+      
+          <div className="flex justify-evenly">
+            <button
+              onClick={()=> {if(counter > 0){setCounter(counter-1)}}}
+              className="bg-blue-600 hover:bg-blue-900 text-black rounded-lg p-3 font-bold"
+            >
+              Decrement
+            </button>
+            <button
+              onClick={() => setCounter(counter + 1)}
+              className="bg-blue-600 hover:bg-blue-900 text-black rounded-lg p-3 font-bold"
+            >
+              Increment
+            </button>
+          </div>
+          <div className="flex justify-center">
+            <button
+              onClick={() => setCounter("0")}
+              className="bg-red-600 hover:bg-red-800 text-black rounded-lg p-3 font-bold flex justify-center"
+            >
+              Reset
+            </button>
           </div>
         </div>
       </div>
     </>
   );
 };
-
 export default Counter;
