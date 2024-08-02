3b29d7d23cb798a7d98f97af43570cd73f42f22f
gsmSahani <sahanigautam0446162@gmail.com>
2024-08-02 21:15:20 +0530

    star rating

---
+++ Star Rating/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Stars from "./Components/Star";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Stars noOfStars={5} />
+      </div>
+    </>
+  );
+};
+
+export default App;
